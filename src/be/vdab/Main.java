647f47b9144e885fc8a8ef3fac76566629bb25fa
Sam Demeulemeester <sam.demeulemeester@vdabcampus.be>
2018-09-17 12:46:40 +0200

    hallo iedereen

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -1,7 +1,7 @@
 package be.vdab;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour");
+        System.out.println("Hallo iedereen");
     }
     
 }
